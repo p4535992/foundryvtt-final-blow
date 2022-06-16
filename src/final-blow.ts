d91c00c7eba70afb4bcaf44173b71b4e50f178ce
p4535992 <tentimarco0@gmail.com>
2022-06-16 14:23:58 +0200

    beta

---
+++ src/final-blow.ts
@@ -0,0 +1,102 @@
+/**
+ * This is your TypeScript entry file for Foundry VTT.
+ * Register custom settings, sheets, and constants using the Foundry API.
+ * Change this heading to be more descriptive to your module, or remove it.
+ * Author: [your name]
+ * Content License: [copyright and-or license] If using an existing system
+ * 					you may want to put a (link to a) license or copyright
+ * 					notice here (e.g. the OGL).
+ * Software License: [your license] Put your desired license here, which
+ * 					 determines how others may use and modify your module
+ */
+// Import JavaScript modules
+
+// Import TypeScript modules
+import { registerSettings } from './module/settings.js';
+import { initHooks, readyHooks, setupHooks } from './module/module';
+import type { ItemData } from '@league-of-foundry-developers/foundry-vtt-types/src/foundry/common/data/module.mjs.js';
+import { error, i18n, warn } from './module/lib/lib.js';
+import CONSTANTS from './module/constants.js';
+import type API from './module/api.js';
+
+/* ------------------------------------ */
+/* Initialize module					*/
+/* ------------------------------------ */
+Hooks.once('init', async () => {
+	console.log(`${CONSTANTS.MODULE_NAME} | Initializing ${CONSTANTS.MODULE_NAME}`);
+
+	// Register custom module settings
+	registerSettings();
+	initHooks();
+
+  // Preload Handlebars templates
+	//await preloadTemplates();
+
+
+});
+
+/* ------------------------------------ */
+/* Setup module							*/
+/* ------------------------------------ */
+Hooks.once('setup', function () {
+	// Do anything after initialization but before ready
+  setupHooks();
+});
+
+/* ------------------------------------ */
+/* When ready							*/
+/* ------------------------------------ */
+Hooks.once('ready', async () => {
+	// Do anything once the module is ready
+	if (!game.modules.get("lib-wrapper")?.active && game.user?.isGM){
+   	error(`The '${CONSTANTS.MODULE_NAME}' module requires to install and activate the 'libWrapper' module.`, true);
+		return;
+	}
+	readyHooks();
+
+});
+
+/* ------------------------------------ */
+/* Other Hooks							*/
+/* ------------------------------------ */
+
+export interface FinalBlowModuleData {
+  api: typeof API;
+  socket: any;
+}
+
+/**
+ * Initialization helper, to set API.
+ * @param api to set to game module.
+ */
+export function setApi(api: typeof API): void {
+  const data = game.modules.get(CONSTANTS.MODULE_NAME) as unknown as FinalBlowModuleData;
+  data.api = api;
+}
+
+/**
+ * Returns the set API.
+ * @returns Api from games module.
+ */
+export function getApi(): typeof API {
+  const data = game.modules.get(CONSTANTS.MODULE_NAME) as unknown as FinalBlowModuleData;
+  return data.api;
+}
+
+/**
+ * Initialization helper, to set Socket.
+ * @param socket to set to game module.
+ */
+export function setSocket(socket: any): void {
+  const data = game.modules.get(CONSTANTS.MODULE_NAME) as unknown as FinalBlowModuleData;
+  data.socket = socket;
+}
+
+/*
+ * Returns the set socket.
+ * @returns Socket from games module.
+ */
+export function getSocket() {
+  const data = game.modules.get(CONSTANTS.MODULE_NAME) as unknown as FinalBlowModuleData;
+  return data.socket;
+}
--- src/module/module.ts
+++ src/module/module.ts
@@ -1,128 +1,15 @@
 import { warn, error, debug, i18n } from "./lib/lib";
-import { getEmbeddedDocument, createEmbeddedDocuments, deleteEmbeddedDocuments, updateEmbeddedDocuments, prepareEmbeddedEntities, getEmbeddedCollection, _onCreateDocuments, calcPrice, calcWeight, containedItemCount, deleteDocuments, getActor, updateDocuments, calcItemWeight, _update, _delete, prepareDerivedData, isEmbedded, getChatData } from "./ItemContainer";
-import type { ItemData } from "@league-of-foundry-developers/foundry-vtt-types/src/foundry/common/data/data.mjs";
-import CONSTANTS from "./constants";
 
-
-export const readyHooks = async () => {
-  warn("Ready Hooks processing");
-
-}
 
 export const initHooks = () => {
   warn("Init Hooks processing");
-
   // setup all the hooks
-
 }
 
 export const setupHooks = () => {
   warn("Setup Hooks processing");
-  // TODO we cna remove this ????
-  //@ts-ignore
-  libWrapper.ignore_conflicts(CONSTANTS.MODULE_NAME, "VariantEncumbrance", "CONFIG.Item.documentClass.prototype.updateEmbeddedDocuments")
-
-  //@ts-ignore
-  libWrapper.register(CONSTANTS.MODULE_NAME, "CONFIG.Item.documentClass.prototype.getEmbeddedDocument", getEmbeddedDocument, "MIXED")
-  //@ts-ignore
-  libWrapper.register(CONSTANTS.MODULE_NAME, "CONFIG.Item.documentClass.prototype.createEmbeddedDocuments", createEmbeddedDocuments, "MIXED")
-  //@ts-ignore
-  libWrapper.register(CONSTANTS.MODULE_NAME, "CONFIG.Item.documentClass.prototype.deleteEmbeddedDocuments", deleteEmbeddedDocuments, "MIXED")
-  //@ts-ignore
-  libWrapper.register(CONSTANTS.MODULE_NAME, "CONFIG.Item.documentClass.prototype.updateEmbeddedDocuments", updateEmbeddedDocuments, "MIXED")
-  //@ts-ignore
-  if (isNewerVersion(game.version, "0.9.0")) {
-    //@ts-ignore
-    libWrapper.register(CONSTANTS.MODULE_NAME, "CONFIG.Item.documentClass.prototype.prepareEmbeddedDocuments", prepareEmbeddedEntities, "WRAPPER");
-  } else {
-    //@ts-ignore
-    libWrapper.register(CONSTANTS.MODULE_NAME, "CONFIG.Item.documentClass.prototype.prepareEmbeddedEntities", prepareEmbeddedEntities, "WRAPPER");
-  }
-  //@ts-ignore
-  libWrapper.register(CONSTANTS.MODULE_NAME, "CONFIG.Item.documentClass.prototype.getEmbeddedCollection", getEmbeddedCollection, "MIXED")
-  //@ts-ignore
-  libWrapper.register(CONSTANTS.MODULE_NAME, "CONFIG.Item.documentClass.prototype.prepareDerivedData", prepareDerivedData, "WRAPPER");
-  //@ts-ignore
-  libWrapper.register(CONSTANTS.MODULE_NAME, "CONFIG.Item.documentClass.prototype.actor", getActor, "OVERRIDE")
-  //@ts-ignore
-  libWrapper.register(CONSTANTS.MODULE_NAME, "CONFIG.Item.documentClass.prototype.update", _update, "MIXED")
-  //@ts-ignore
-  libWrapper.register(CONSTANTS.MODULE_NAME, "CONFIG.Item.documentClass.prototype.delete", _delete, "MIXED")
-  //@ts-ignore
-  libWrapper.register(CONSTANTS.MODULE_NAME, "CONFIG.Item.documentClass.prototype.isEmbedded", isEmbedded, "OVERRIDE")
-
-  //@ts-ignore
-  libWrapper.register(CONSTANTS.MODULE_NAME, "CONFIG.Item.documentClass._onCreateDocuments", _onCreateDocuments, "MIXED")
-  //@ts-ignore
-  libWrapper.register(CONSTANTS.MODULE_NAME, "CONFIG.Item.documentClass.deleteDocuments", deleteDocuments, "MIXED")
-  //@ts-ignore
-  libWrapper.register(CONSTANTS.MODULE_NAME, "CONFIG.Item.documentClass.updateDocuments", updateDocuments, "MIXED")
-  //@ts-ignore
-  libWrapper.register(CONSTANTS.MODULE_NAME, "CONFIG.Item.documentClass.prototype.getChatData", getChatData, "WRAPPER")
-
-  //@ts-ignore documentClass
-  CONFIG.Item.documentClass.prototype.calcWeight = calcWeight;
-  //@ts-ignore documentClass
-  CONFIG.Item.documentClass.prototype.calcItemWeight = calcItemWeight;
-  //@ts-ignore documentClass
-  CONFIG.Item.documentClass.prototype.calcPrice = calcPrice;
-  //@ts-ignore documentClass
-  CONFIG.Item.documentClass.prototype.containedItemCount = containedItemCount;
-
-  Hooks.on("preCreateItem", (candidate:Item, data, options, user) => {
-    if (!(candidate instanceof Item
-          && candidate.type === "backpack"
-          && data.flags?.itemcollection
-          && candidate.getFlag(CONSTANTS.MODULE_NAME,'version') !== "0.8.6"))
-        return true;
-    if (data.flags.itemcollection?.contents && data.flags.itemcollection?.version !== "0.8.6") { // old version to convert
-      const itemcollectionData = {
-        contentsData: duplicate(data.flags.itemcollection.contents || []),
-        version: "0.8.6",
-        bagWeight: data.flags.itemcollection?.fixedWeight ?? 0,
-        bagPrice: data.data.price ?? 0
-      };
-      itemcollectionData.contentsData.forEach(itemData => {
-        itemData._id = randomID();
-        (itemData.effects ?? []).forEach(effectData => {
-          effectData.origin = undefined;
-        })
-        if (itemData.type === "backpack") fixupItemData(itemData);
-      })
-      candidate.data.update({
-        "flags.itemcollection.-=contents": null,
-        "flags.itemcollection.-=goldValue": null,
-        "flags.itemcollection.-=fixedWeight": null,
-        "flags.itemcollection.-=importSpells": null,
-        "flags.itemcollection.-=itemWeight": null
-      });
-      candidate.data.update({"flags.itemcollection": itemcollectionData});
-
-    }
-  });
-
-  Hooks.on("updateItem", (item, updates, options, user) => {
-  });
-
 }
 
-export function fixupItemData(itemData:any) {
-  if (!itemData.flags.itemcollection || itemData.flags.itemcollection.version === "0.8.6") return;
-  const itemcontents = duplicate(itemData.flags.itemcollection.contents || []);
-  for (const iidata of itemcontents) {
-    iidata._id = randomID();
-    (iidata.effects ?? []).forEach(effectData => {
-      effectData.origin = undefined;
-    });
-    if (iidata.type === "backpack") fixupItemData(iidata);
-  }
-  itemData.flags.itemcollection.version = "0.8.6";
-  itemData.flags.itemcollection.bagWeight = itemData.flags.itemcollection?.fixedWeight ?? 0;
-  itemData.flags.itemcollection.bagPrice = itemData.data.price ?? 0;
-  itemData.flags.itemcollection.contentsData = itemcontents;
-  delete itemData.flags.itemcollection.contents
-  delete itemData.flags.itemcollection.goldValue;
-  delete itemData.flags.itemcollection.fixedWeight;
-  delete itemData.flags.itemcollection.importSpells;
-  delete itemData.flags.itemcollection.itemWeight;
+export const readyHooks = async () => {
+  warn("Ready Hooks processing");
 }
